1c254e482c243509c9b51ef71a3fe9a6bea40fad
Kelsey Florek <kelseyflorek22@gmail.com>
2021-03-23 17:54:49 -0500

    added a few more plots, fixed content length

--- sc2_variant_freq.R
+++ sc2_variant_freq.R
@@ -1,11 +1,12 @@
 library(plotly)
 library(dplyr)
 
+#render a plot of the cumulative total of sequences in GISAID
 renderTotal <- function(sc2Data){
   # load SC2 data
   sc2byDate <- data.frame(table(sc2Data$Collection.date))
   names(sc2byDate) <- c("date","num")
-  sc2byDate <- sc2byDate[!(sc2byDate$date=="2020"),]
+  sc2byDate <- sc2byDate[!(sc2byDate$date=="2020"|sc2byDate$date=="2021"),]
 
   fig <- plot_ly()
   # total number
@@ -27,10 +28,26 @@ renderTotal <- function(sc2Data){
     )
   return(fig)
 }
+
+#render a plot of the proportion lineages by week
+renderTimeTotal <- function(data){
+  fig <- plot_ly()
+  # total number
+  fig <- fig %>% add_trace(
+    type = "bar",
+    x = data$date,
+    y = data$num,
+    name = 'Total'
+  )
+  return(fig)
+}
+
+
+#render a plot of the variants of concern
 renderVOC <- function(sc2Data){
   sc2bylineage <- data.frame(table(sc2Data$Collection.date,sc2Data$Lineage))
   names(sc2bylineage) <- c("date","lineage","num")
-  sc2bylineage <- sc2bylineage[!(sc2bylineage$date=="2020"),]
+  sc2bylineage <- sc2bylineage[!(sc2bylineage$date=="2020"|sc2bylineage$date=="2021"),]
 
   fig <- plot_ly()
   # b.1.1.7
@@ -52,6 +69,18 @@ renderVOC <- function(sc2Data){
     x = as.Date(data$date, format= "%Y-%m-%d"),
     y = cumsum(data$num),
     name = 'B.1.351',
+    mode = "lines"
+  )
+  
+  #CAL.20C (B.1.429 & B.1.427)
+  data <- data.frame(date="2020-01-01",lineage.x="B.1.429",num.x=0,lineage.y="B.1.427",num.y=0)
+  data <- rbind(data,merge(sc2bylineage[sc2bylineage$lineage == "B.1.429",], sc2bylineage[sc2bylineage$lineage == "B.1.427",],by="date"))
+  data$num <- data$num.x + data$num.y
+  fig <- fig %>% add_trace(
+    type = "scatter",
+    x = as.Date(data$date, format= "%Y-%m-%d"),
+    y = cumsum(data$num),
+    name = 'CAL.20C (B.1.429 & B.1.427)',
     mode = "lines"
   )
 
@@ -77,10 +106,11 @@ renderVOC <- function(sc2Data){
   return(fig)
 }
 
+#render a plot of the variants of interest
 renderVOI <- function(sc2Data){
   sc2bylineage <- data.frame(table(sc2Data$Collection.date,sc2Data$Lineage))
   names(sc2bylineage) <- c("date","lineage","num")
-  sc2bylineage <- sc2bylineage[!(sc2bylineage$date=="2020"),]
+  sc2bylineage <- sc2bylineage[!(sc2bylineage$date=="2020"|sc2bylineage$date=="2021"),]
   
   fig <- plot_ly()
   # P.2
@@ -93,19 +123,7 @@ renderVOI <- function(sc2Data){
     name = 'P.2',
     mode = "lines"
   )
-  
-  #CAL.20C (B.1.429 & B.1.427)
-  data <- data.frame(date="2020-01-01",lineage.x="B.1.429",num.x=0,lineage.y="B.1.427",num.y=0)
-  data <- rbind(data,merge(sc2bylineage[sc2bylineage$lineage == "B.1.429",], sc2bylineage[sc2bylineage$lineage == "B.1.427",],by="date"))
-  data$num <- data$num.x + data$num.y
-  fig <- fig %>% add_trace(
-    type = "scatter",
-    x = as.Date(data$date, format= "%Y-%m-%d"),
-    y = cumsum(data$num),
-    name = 'CAL.20C (B.1.429 & B.1.427)',
-    mode = "lines"
-  )
-  
+
   # B.1.525
   data <- data.frame(date="2020-01-01",lineage="B.1.525",num=0)
   data <- rbind(data,sc2bylineage[sc2bylineage$lineage == "B.1.525",])
@@ -125,28 +143,6 @@ renderVOI <- function(sc2Data){
     x = as.Date(data$date, format= "%Y-%m-%d"),
     y = cumsum(data$num),
     name = 'B.1.526',
-    mode = "lines"
-  )
-
-  # A.23.1
-  data <- data.frame(date="2020-01-01",lineage="A.23.1",num=0)
-  data <- rbind(data,sc2bylineage[sc2bylineage$lineage == "A.23.1",])
-  fig <- fig %>% add_trace(
-    type = "scatter",
-    x = as.Date(data$date, format= "%Y-%m-%d"),
-    y = cumsum(data$num),
-    name = 'A.23.1',
-    mode = "lines"
-  )
-  
-  # B.1.1.318
-  data <- data.frame(date="2020-01-01",lineage="B.1.1.318",num=0)
-  data <- rbind(data,sc2bylineage[sc2bylineage$lineage == "B.1.1.318",])
-  fig <- fig %>% add_trace(
-    type = "scatter",
-    x = as.Date(data$date, format= "%Y-%m-%d"),
-    y = cumsum(data$num),
-    name = 'B.1.1.318',
     mode = "lines"
   )
   
