e08ea0215e1b14ef68eed83c788d0f0a79a8c9c0
Kelsey Florek <kelseyflorek22@gmail.com>
2021-05-11 10:45:37 -0500

    fixed favicon

--- ui.R
+++ ui.R
@@ -61,7 +61,7 @@ fluidPage(
   tags$link(rel = "stylesheet",type = "text/css", href = "wslh-theme/wslh.css"),
   titlePanel(
     title=tags$div(class="titlePanel",tags$a(href='https://dataportal.slh.wisc.edu/',tags$img(src='wslh-theme/wslh-logo.png', height = 90))),
-    tags$head(tags$link(rel = "icon", type = "image/png", href = "wslh-theme/w-logo.png"), windowTitle="WI SARS-CoV-2 Genomic Report")
+    tags$head(tags$link(rel = "icon", href = "wslh-theme/w-favicon.ico"), windowTitle="WI SARS-CoV-2 Genomic Report")
   ),
   navbarPage(id='navtabs',tags$h3("Wisconsin SARS-CoV-2 Genomic Dashboard",style="margin:0px"),
     tabPanel("Sequencing Report",
