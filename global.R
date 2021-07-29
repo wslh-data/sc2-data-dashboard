1a7c0e0d286dca448bfa4da4f785e166c2ca6fb8
Kelsey Florek <kelseyflorek22@gmail.com>
2021-07-29 16:44:27 -0500

    updates to how data is loaded and processed

--- global.R
+++ global.R
@@ -3,6 +3,11 @@ library(plotly)
 library(rjson)
 library(dplyr)
 library(shinydashboard)
+library(highcharter)
+library(shinycssloaders)
+library(shinyWidgets)
+library(shinyBS)
+library(stringr)
 
 source("cumulativeSequences.R")
 source("plotVOC.R")
@@ -11,14 +16,52 @@ source("timeframedata.R")
 source("proportionLineageData.R")
 source("proportionVariantData.R")
 source("sequenceMap.R")
-source("loadGlobalData.R")
+source("dataProcessing.R")
+source("retrieve_data.R")
+source("valueBoxes.R")
+
+### Set VOC/VOI List
+VOI_list <<- c(
+  "B.1.525",
+  "B.1.526",
+  "B.1.526.1",
+  "B.1.617",
+  "B.1.617.1",
+  "B.1.617.3",
+  "B.1.427/429"
+)
+
+VOC_list <<- c(
+  "B.1.1.7",
+  "B.1.351",
+  "B.1.351.2",
+  "B.1.351.3",
+  "P.1",
+  "P.1.1",
+  "P.1.2",
+  "B.1.617.2",
+  "AY.1",
+  "AY.2",
+  "AY.3"
+)
 
 ### Set timezone so our update clock makes sense
 Sys.setenv(TZ='America/Chicago')
+lastUpdate <<- format(Sys.time(),"%Y-%m-%d")
 
-### path for geojsons
-rootPath <<- '/data'
+### load data
+# GeoJSON Files
+WICounty_geojson <<- fromJSON(file=file.path("geojsons","us-counties-fips.json"))
+HERC_geojson <<- fromJSON(file=file.path("geojsons","WI_HERC.json"))
+
+# GISAID Data
+sc2Data <<- get_GISAID_Metadata_data()
+sc2Data <<- unique(sc2Data,by="GISAID_ID")
+sc2Data <<- sc2Data[!is.na(sc2Data$GISAID_ID),]
+# 
+# # DHS Data
+dhsdata <<- get_DHS_county_data()
 
 ### load data and set last update date
-loadGlobalData(rootPath)
-lastUpdate <<- format(Sys.time(),"%Y-%m-%d")
+preProcess()
+generateValueBoxPlots()
