96700ea6c1caf4526bdaa755adcbacb6d9bb7dba
Kelsey Florek <kelseyflorek22@gmail.com>
2021-10-04 16:07:13 -0500

    updated variant lists and removed highcharter

--- global.R
+++ global.R
@@ -3,84 +3,56 @@ library(plotly)
 library(rjson)
 library(dplyr)
 library(shinydashboard)
-library(highcharter)
 library(shinycssloaders)
 library(shinyWidgets)
 library(shinyBS)
 library(stringr)
+library(paws)
 
-source("cumulativeSequences.R")
-source("plotVOC.R")
-source("plotVOI.R")
-source("timeframedata.R")
-source("proportionLineageData.R")
-source("proportionVariantData.R")
-source("sequenceMap.R")
-source("dataProcessing.R")
-source("retrieve_data.R")
-source("valueBoxes.R")
+source("lib/cumulativeSequences.R")
+source("lib/plotVariantCurve.R")
+source("lib/timeframedata.R")
+source("lib/proportionLineageData.R")
+source("lib/proportionVariantData.R")
+source("lib/sequenceMap.R")
+source("lib/dataProcessing.R")
+source("lib/retrieve_data.R")
+source("lib/valueBoxes.R")
+source("lib/county_to_herc.R")
 
-### Set VOC/VOI List
-VOI_list <<- c(
-  "B.1.525",
-  "B.1.526",
-  "C.37",
-  "C.37.1",
-  "B.1.617.1",
-  "B.1.617.3",
-  "B.1.621",
-  "B.1.621.1"
-)
-
-VOC_list <<- c(
-  "B.1.1.7",
-  "B.1.351",
-  "B.1.351.2",
-  "B.1.351.3",
-  "B.1.617.2",
-  "AY.1",
-  "AY.2",
-  "AY.3",
-  "AY.3.1",
-  "AY.4",
-  "AY.5",
-  "AY.5.1",
-  "AY.5.2",
-  "AY.6",
-  "AY.7",
-  "AY.7.1",
-  "AY.7.2",
-  "AY.8",
-  "AY.9",
-  "AY.10",
-  "AY.11",
-  "AY.12",
-  "AY.13",
-  "AY.14",
-  "AY.15",
-  "AY.16",
-  "AY.17",
-  "AY.18",
-  "AY.19",
-  "AY.20",
-  "AY.21",
-  "AY.22",
-  "AY.23",
-  "AY.24",
-  "AY.25",
-  "P.1",
-  "P.1.1",
-  "P.1.2"
+### Set VOC/VOI List and WHO Lineages
+WHO_list <<- list(
+  'Alpha' = c("B.1.1.7","Q.1","Q.2","Q.3","Q.4","Q.5","Q.6",
+              "Q.7","Q.8"),
+  'Beta' = c("B.1.351","B.1.351.2","B.1.351.3"),
+  'Gamma' = c("P.1","P.1.1","P.1.2"),
+  'Delta' = c("B.1.617.2","AY.1","AY.2","AY.3","AY.3.1","AY.4","AY.4.1",
+             "AY.5","AY.5.1","AY.5.2","AY.6","AY.7","AY.7.1",
+             "AY.7.2","AY.8","AY.9","AY.10","AY.11","AY.12",
+             "AY.13","AY.14","AY.15","AY.16","AY.17","AY.18",
+             "AY.19","AY.20","AY.21","AY.22","AY.23","AY.24",
+             "AY.25","AY.26","AY.27","AY.28","AY.29","AY.30",
+             "AY.31","AY.32","AY.33","AY.34","AY.35","AY.36",
+             "AY.37","AY.38"),
+  'Epsilon' = c("B.1.427","B.1.429"),
+  'Eta' = c("B.1.525"),
+  'Iota' = c("B.1.526"),
+  'Kappa' = c("B.1.617.1"),
+  'Lambda' = c("C.37","C.37.1"),
+  'Mu' = c("B.1.621","B.1.621.1"),
+  'Zeta' = c("P.2")
 )
 
 WHO_VOC <<- c(
-  'Alpha',
-  'Beta',
-  'Gamma',
   'Delta'
 )
 
-WHO_VOI <<- c(
+WHO_VBM <<- c(
+  'Alpha',
+  'Beta',
+  'Gamma',
+  'Epsilon',
+  'Zeta',
   'Eta',
   'Iota',
   'Kappa',
@@ -88,22 +60,11 @@ WHO_VOI <<- c(
   'Mu'
 )
 
-WHO_list <<- list(
-  'Alpha' = c('B.1.1.7'),
-  'Beta' = c("B.1.351","B.1.351.2","B.1.351.3"),
-  'Gamma' = c("P.1","P.1.1","P.1.2"),
-  'Delta' = c("B.1.617.2","AY.1","AY.2","AY.3","AY.3.1","AY.4",
-             "AY.5","AY.5.1","AY.5.2","AY.6","AY.7","AY.7.1",
-             "AY.7.2","AY.8","AY.9","AY.10","AY.11","AY.12",
-             "AY.13","AY.14","AY.15","AY.16","AY.17","AY.18",
-             "AY.19","AY.20","AY.21","AY.22","AY.23","AY.24",
-             "AY.25"),
-  'Eta' = c("B.1.525"),
-  'Iota' = c("B.1.526"),
-  'Kappa' = c("B.1.617.1"),
-  'Lambda' = c("C.37","C.37.1"),
-  'Mu' = c("B.1.621","B.1.621.1")
-)
+VOC_list <<- WHO_list$Delta
+
+VBM_list <<- c("B.1.617.3", WHO_list$Alpha, WHO_list$Beta, WHO_list$Gamma,
+               WHO_list$Epsilon, WHO_list$Eta, WHO_list$Iota, WHO_list$Kappa,
+               WHO_list$Mu, WHO_list$Zeta)
 
 ### Function to convert lineage to WHO name
 getWHO <<- function(x){
@@ -114,7 +75,6 @@ getWHO <<- function(x){
   }
   return("Other")
 }
-
 
 ### Set timezone so our update clock makes sense
 Sys.setenv(TZ='America/Chicago')
