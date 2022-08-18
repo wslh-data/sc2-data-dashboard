2d895a050929f6d08c9330f28d6524cedbdfc796
Kelsey Florek <kelseyflorek22@gmail.com>
2022-08-18 15:30:57 -0500

    fixed date selection step

--- variants/app.R
+++ variants/app.R
@@ -42,6 +42,7 @@ ui <- fluidPage(
       width = '100%',
       min = floor_date(as.Date('2020-01-01',"%Y-%m-%d"), unit='week', week_start = 1),
       max = floor_date(as.Date(format(Sys.Date(),"%Y-%m-%d")), unit='week', week_start = 1),
+      step=7,
       value = c(
        floor_date(seq(as.Date(format(Sys.Date(),"%Y-%m-%d")), length = 2, by = "-6 months")[2], unit='week', week_start = 1),
        floor_date(seq(as.Date(format(Sys.Date(),"%Y-%m-%d")), length = 2, by = "-2 weeks")[2], unit='week', week_start = 1)
